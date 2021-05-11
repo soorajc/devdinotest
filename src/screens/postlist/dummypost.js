920d8ad5b5843d753ba13f27b4730596dc89f86b
soorajc <soorajcmpm@gmail.com>
2021-05-11 12:57:28 +0530

    Removed: invalid dates in dummy post data

--- src/screens/postlist/dummypost.js
+++ src/screens/postlist/dummypost.js
@@ -5,11 +5,11 @@ const posts = {
       user: 'jhon',
       title: 'Spring Break Destinations 2021',
       body: 'Hey spring breakers – I’m sure you’re wondering where exactly you should go this upcoming spring vacation. Whether you want to party under the sun or ski in the snow, here are TourMega’s top 10 favorite spring break destinations just for you and your friends!',
-      time: '10/02/2021 4:30 pm',
+      time: '01/02/2021 4:30 pm',
       comments: [
         {
           id: '#c1',
-          time: '10/02/2021 4:32 pm',
+          time: '01/02/2021 4:32 pm',
           user: 'mersy',
           comment: 'Thank you for sharing 😀',
         },
@@ -20,11 +20,11 @@ const posts = {
       user: 'sam',
       title: 'Festival Events in 2021 – April Update',
       body: 'April is finally here – a time when we look forward to festival lineup releases and outdoor events! With a steady reopening of California post-covid, it is difficult to know exactly which festivals are still on schedule or not. Here is all you need to know about the upcoming events in California for the month of April and later in the year!',
-      time: '10/02/2021 4:30 pm',
+      time: '01/03/2021 4:30 pm',
       comments: [
         {
           id: '#c3',
-          time: '10/02/2021 5:32 pm',
+          time: '01/03/2021 5:32 pm',
           user: 'maci',
           comment: 'Yes I would like to know more.',
         },
@@ -37,11 +37,11 @@ const posts = {
       user: 'Support Manager',
       title: 'Travel Bag Essentials',
       body: 'Given the current state of the world, travelers are looking to re-evaluate what safety measures are needed in order to get back on the road, and it all starts with packing the right bag.',
-      time: '11/04/2022 5:30 pm',
+      time: '02/04/2021 5:30 pm',
       comments: [
         {
           id: '#c4',
-          time: '10/02/2021 7:12 pm',
+          time: '02/04/2021 7:12 pm',
           user: 'sego',
           comment: 'Very usefull, thank you',
         },
@@ -52,11 +52,11 @@ const posts = {
       user: 'Sam Chris',
       title: 'Solo Travel 101',
       body: 'Solo travel is the new path to a self-discovering journey. We crave freedom and adventure, yet we somehow end up feeling restrained everytime our friend cancels on a trip',
-      time: '11/04/2022 5:30 pm',
+      time: '02/05/2021 5:30 pm',
       comments: [
         {
           id: '#c4',
-          time: '10/02/2021 1:12 pm',
+          time: '02/05/2021 11:12 pm',
           user: 'ann',
           comment: 'Please share more details',
         },
@@ -69,11 +69,11 @@ const posts = {
       user: 'Admin',
       title: 'We have changed the policy',
       body: 'In this forum only Admin user is allowed to post',
-      time: '09/12/2021 2:30 am',
+      time: '04/01/2021 2:30 am',
       comments: [
         {
           id: '#c3',
-          time: '11/02/2021 3:32 pm',
+          time: '04/01/2021 3:32 pm',
           user: 'sophie',
           comment: 'Thats good it will avoid spams',
         },
@@ -84,11 +84,11 @@ const posts = {
       user: 'Admin',
       title: 'Email notifications intergrated',
       body: 'Users will be notified through email from next week onwards',
-      time: '09/12/2021 2:30 am',
+      time: '04/03/2021 5:30 am',
       comments: [
         {
           id: '#c5',
-          time: '13/02/2021 3:32 pm',
+          time: '04/03/2021 3:32 pm',
           user: 'camy',
           comment: 'how to enable it?',
         },
