9bf08e39c28a4c112fb0b4be0222c51ae54065c2
Soorajc <soorajcmpm@gmail.com>
2021-07-23 21:44:53 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const appConfig = {
+  title: 'Welcome To Student App',
+  backgroundColor: 'white',
+  loginButtonColor: 'red',
+  loginButtonLabel: 'Student Login',
+  signUpButtonLabel: 'Create Student',
+  signUpButtonColor: '#7f32fc',
+};
+
+export default appConfig;
